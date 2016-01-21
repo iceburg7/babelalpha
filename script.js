9992199e67b45214c5cf449b78eac9ca40ed8007
iceburg7 <ecburgess1@gmail.com>
2016-01-21 01:52:34 -0500

    first intro done

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
 	//On clicking on the screen, change to the next image.
 	
 	$(document).on("click",".wrapper",function(){
-		if(introstate < 8){
+		if(introstate < 10){
 			switch(introstate){
 				case 0:
 					i("title2.jpg");
@@ -50,13 +50,38 @@ $(document).ready(function(){
 					introstate = 2;
 					break;
 				case 2:
-					show(".textbox");
+					show(".subtitletextbox");
 					introstate = 3;
 					break;
 				case 3:
-					$(".textbox").css("color", "black")
+					$(".subtitletextbox").css("color", "black")
 					i("babel-outside.gif");
 					introstate = 4;
+					break;
+				case 4:
+					show(".textboxleft");
+					show(".textboxright");
+					introstate = 5;
+					break;
+				case 5:
+					$(".textboxleft").append("<br><br>First, myself.  Mostly as I knew myself to be,<br> though my eyesight had deteriorated significantly.");
+					introstate = 6;
+					break;
+				case 6:
+					$(".textboxright").append("<br><br>Second, a tiny copy of myself, no larger than<br>the palm of my hand.");
+					introstate = 7;
+					break;
+				case 7:
+					$(".textboxleft").append("<br><br>This tiny man was identical to me in every aspect <br>save for size; but he possessed two things that I did not:          ");
+					introstate = 8;
+					break;
+				case 8:
+					$(".textboxright").append("<br><br>He still maintained his full eyesight, or perhaps he had a tiny pair of spectacles.");
+					introstate = 9;
+					break;
+				case 9:
+					$(".textboxleft").append("<br><br>And he had a plan.<br>  Our separation must have been in error, and in order<br>to reunite ourselves and set things right,<br>we must travel to the center of Babel Alpha.");
+					introstate = 10;
 					break;
 			}
 		}
